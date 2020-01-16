fbdf8d745215ef3474e1a33ac18eca73c049a9d1
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-16 23:39:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
-# Your Code Here
+
+def map(source)
+  new_arr = []
+  source.each do |val|
+    new_arr << yield(val)
+  end
+  return new_arr
+end
+
+# making a general-case initial value is
+# a strange and ambiguous thing
+def reduce(source, initial = nil)
+  if !initial then
+    result = source[0]
+    index = 1
+  else
+    result = initial
+    index = 0
+  end
+  
+  while index < source.length do
+    result = yield(result, source[index])
+    index += 1
+  end
+  
+  return result
+end
+
+puts (0 && true)
