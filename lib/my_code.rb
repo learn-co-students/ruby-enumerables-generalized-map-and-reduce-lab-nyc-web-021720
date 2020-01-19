99dd78044003b84193a235c9bc8117a3c1b2ef5f
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-19 20:38:35 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
 # Your Code Here
+arr = [1,2,3,4]
+def map(source_array)
+  result =[]
+  source_array.each do |element|
+    result<<yield(element)
+  end
+  result
+end
+def reduce(source_array,starting_point=0)
+  result = 0
+  source_array.each do |element|
+    result =
+  end
+end
