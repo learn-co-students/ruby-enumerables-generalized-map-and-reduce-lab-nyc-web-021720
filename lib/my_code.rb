f1f1418dec180822f89b9b5f794b698802338f11
Brian Desplantes <heftygac@gmail.com>
2020-02-11 15:55:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map (array)
+ map_arr = []
+
+array.each do |x|
+	map_arr.push(yield x)
+ end
+
+ return map_arr
+end
+
+def reduce (array, starting_point = 0)
+  sum = starting_point
+
+  array.each do |value|
+    sum = yield(sum, value)
+
+    if (!value)
+      return false
+    end
+  end
+
+
+
+  return sum
+end
